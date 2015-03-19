9e73cb0e176bd56d5b876c62d78a8d820cf2da4f
Raj <Raj@Raj-PC>
2015-03-18 21:00:23 -0500

    spring security finalised.

--- UniversityStore/src/mum/universitystore/controller/LoginController.java
+++ UniversityStore/src/mum/universitystore/controller/LoginController.java
@@ -10,7 +10,7 @@ public class LoginController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
-		return "welcome";
+		return "login";
 	}
 
 	@RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
